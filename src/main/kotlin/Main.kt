0e3c5dc9177fc40856651d8a12056efd6468edb2
Oskar Schulz <vollkorncrafter@gmail.com>
2024-06-07 00:12:49 +0200

    some changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +16,8 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.key.Key.Companion.R
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,9 +53,8 @@ fun NumberBox(number: String) {
     }
 }
 
-
 @Composable
-fun CustomCheckBox(isChecked: Boolean, party: ElectionListItem, onCheckedChange: () -> Unit) {
+fun CustomCheckBox(isChecked: Boolean, party: ElectionListItem, onCheckedChange: (ElectionListItem?) -> Unit) {
     Box(
         modifier = Modifier
             .width(50.dp)
@@ -60,7 +62,7 @@ fun CustomCheckBox(isChecked: Boolean, party: ElectionListItem, onCheckedChange:
             .border(2.dp, Color.Black)
             .padding(8.dp)
             .clickable {
-                onCheckedChange()
+                onCheckedChange(if (isChecked) null else party)
             },
         contentAlignment = Alignment.Center
     ) {
@@ -96,13 +98,11 @@ fun DrawCircle(isChecked: Boolean) {
                 strokeWidth = 4f
             )
         }
-
     }
 }
 
-
 @Composable
-fun partei(party: ElectionListItem, selectedParty: ElectionListItem?, onPartySelected: (ElectionListItem) -> Unit) {
+fun partei(party: ElectionListItem, selectedParty: ElectionListItem?, onPartySelected: (ElectionListItem?) -> Unit) {
     val isSelected = selectedParty == party
     Row(
         modifier = Modifier.background(color = Color.White).width(910.dp).border(2.dp, SolidColor(Color.Black), shape = RoundedCornerShape(0.dp))
@@ -111,21 +111,25 @@ fun partei(party: ElectionListItem, selectedParty: ElectionListItem?, onPartySel
         Column(
             modifier = Modifier.background(color = Color.Cyan).width(800.dp)
         ) {
-            Row(modifier = Modifier.background(color = Color.White).fillMaxWidth().height(40.dp), Arrangement.SpaceBetween, verticalAlignment = Alignment.Bottom) {
-                Text(party.shortName, style = MaterialTheme.typography.h5)
-                Text(party.fullName)
-                Text(party.electionList)
+            Row(modifier = Modifier.background(color = Color.White).fillMaxWidth().height(40.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(party.shortName, style = MaterialTheme.typography.h5, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(party.fullName, fontWeight = FontWeight.Bold)
+                }
+                Text("- " + party.electionList + " -", modifier = Modifier.align(Alignment.CenterVertically), fontWeight = FontWeight.Bold)
             }
             Row(modifier = Modifier.background(color = Color.White).fillMaxWidth().height(100.dp), verticalAlignment = Alignment.Bottom) {
                 Column(
                     modifier = Modifier.background(color = Color.White).width(400.dp).padding(1.dp)
                 ) {
+
                     party.candidates.take(5).forEach { candidate ->
                         Text(
-                            if(candidate.occupation != null)
-                                candidate.name + ", " + candidate.occupation
+                            if (candidate.occupation != null)
+                               candidate.listPosition.toString() + ". " + candidate.name + ", " + candidate.occupation
                             else
-                                candidate.name
+                                candidate.listPosition.toString() + ". " +candidate.name
                         )
                     }
                 }
@@ -134,16 +138,18 @@ fun partei(party: ElectionListItem, selectedParty: ElectionListItem?, onPartySel
                 ) {
                     party.candidates.drop(5).take(5).forEach { candidate ->
                         Text(
-                            if(candidate.occupation != null)
-                                candidate.name + ", " + candidate.occupation
+                            if (candidate.occupation != null)
+                                candidate.listPosition.toString() + ". " + candidate.name + ", " + candidate.occupation
                             else
-                                candidate.name
+                                candidate.listPosition.toString() + ". " +candidate.name
                         )
                     }
                 }
             }
         }
-        CustomCheckBox(isSelected, party) { onPartySelected(party) }
+        CustomCheckBox(isSelected, party) { selected ->
+            onPartySelected(selected)
+        }
     }
 }
 
@@ -170,8 +176,6 @@ fun Example(onClick: (String) -> Unit) {
     }
 }
 
-
-
 @OptIn(ExperimentalLayoutApi::class)
 fun main() = application {
     val electionList = Json.decodeFromString<ElectionList>(readFileDirectlyAsText("election_list.json"))
@@ -183,7 +187,12 @@ fun main() = application {
     val Config = Json.decodeFromString<Config>(readFileDirectlyAsText("config.json"))
     val password = Config.password
 
-    Window(onCloseRequest = ::exitApplication) {
+    var windowTitle by remember { mutableStateOf("Europawahl") }
+
+    Window(title = windowTitle,
+        resizable = true,
+        onCloseRequest = ::exitApplication ,
+          ) {
 
         Box(
             modifier = Modifier.fillMaxSize(),
@@ -200,24 +209,25 @@ fun main() = application {
                             fontSize = 48.sp,
                             fontWeight = FontWeight.Bold
                         )
-                    Text(
-                        text = "für die Wahl der Abgeordneten des Europäischen Parlaments am 9.Juni 2024",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = "im Land Nordrhein-Westfalen",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = "Sie haben 1 Stimme",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold
-                    )}
+                        Text(
+                            text = "für die Wahl der Abgeordneten des Europäischen Parlaments am 9.Juni 2024",
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "im Land Nordrhein-Westfalen",
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "Sie haben 1 Stimme",
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                     items(sortedElectionList.size) { index ->
                         partei(
-                            sortedElectionList.get(index),
+                            sortedElectionList[index],
                             selectedParty,
                             onPartySelected = { selectedParty = it }
                         )
@@ -236,7 +246,7 @@ fun main() = application {
                     Button(
                         onClick = {
                             selectedParty = null
-                            if(verificationCode == password) {
+                            if (verificationCode == password) {
                                 showVerification = false
                             }
                             verificationCode = ""
@@ -246,28 +256,27 @@ fun main() = application {
                         colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
                     ) {
                         Text("Verify", color = Color.White)
-
                     }
                 }
             }
         }
-        if(!showVerification){
-        Example { timestamp ->
-            val selectedParty = selectedParty?.shortName ?: ""
-            val votedParty = VotedParty(selectedParty, timestamp)
-            writeToCsv(votedParty)
-            showVerification = true
-        }}
+        if (!showVerification) {
+            Example { timestamp ->
+                val selectedPartyName = selectedParty?.shortName ?: ""
+                val votedParty = VotedParty(selectedPartyName, timestamp)
+                writeToCsv(votedParty)
+                selectedParty = null // Reset the selected party
+                showVerification = true
+            }
+        }
     }
 }
-
-
 
 fun writeToCsv(votedParty: VotedParty) {
     val fileName = "voted_parties.csv"
     val file = File(fileName)
     val exists = file.exists()
-    val header = listOf("Party",  "Timestamp")
+    val header = listOf("Party", "Timestamp")
     val row = listOf(votedParty.party, votedParty.timestamp)
 
     if (!exists) {
